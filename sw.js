b4eaff944bf1a96e01e7771dad2092d7fa586cb0
7tqp7y9h7p-byte <7tqp7y9h7p@privaterelay.appleid.com>
2026-02-02 15:10:03 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,7 @@
-const CACHE = "markov-offline-v1";
-
-self.addEventListener("install", e => {
- e.waitUntil(
-  caches.open(CACHE).then(c =>
-   c.addAll(["./","index.html"])
-  )
- );
+const CACHE="markov-offline-v1";
+self.addEventListener("install",e=>{
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","index.html"])))
 });
-
-self.addEventListener("fetch", e => {
- e.respondWith(
-  caches.match(e.request).then(r => r || fetch(e.request))
- );
+self.addEventListener("fetch",e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
 });
